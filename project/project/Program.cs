fcf650c4c3fcb2cfe47b75c1785d7b2c6625acc9
friedmanrivka <hand1c26@mbjcomp.org.il>
2024-05-03 02:18:10 +0300

    start to do the part of branch in the program

--- project/project/Program.cs
+++ project/project/Program.cs
@@ -9,19 +9,36 @@ using static System.Console;
 User user1 = new("Sari Choen", "123456789", "sari727896@gmail.com");
 User user2 = new("Rivky Friedman", "987654321", "rivka0504173291@gmail.com");
 User user3 = new("Malki Domb", "246813579", "md382@gmail.com");
+User user4 = new User("David Levi", "111222333", "davidlevi@example.com");
+User user5 = new User("Rachel Cohen", "444555666", "rachelc@example.com");
+User user6 = new User("Yossi Goldberg", "777888999", "yossig@example.com");
+User user7 = new User("Esther Schwartz", "333222111", "esthers@example.com");
+User user8 = new User("Avi Cohen", "666777888", "avic@example.com");
+User user9 = new User("Leah Rosenberg", "999888777", "leahr@example.com");
+User user10 = new User("Yitzhak Green", "123123123", "yitzhakg@example.com");
 GitApp gitApp = GitApp.GetInstance();
 gitApp.ChangeCurrentUser(user1);
 gitApp.AddUser(user2);
 gitApp.AddUser(user3);
+gitApp.AddUser(user4);
+gitApp.AddUser(user5);
+gitApp.AddUser(user6);
+gitApp.AddUser(user7);
+gitApp.AddUser(user8);
+gitApp.AddUser(user9);
+gitApp.AddUser(user10);
 Branch branchMain = new("main");
-Branch branch2 = new("fitcher", branchMain);
+Branch branch2 = new("Feature branch", branchMain);
+Branch branch3= new("Release  branch", branchMain);
+Branch branch4 = new("Hotfix  branch", branchMain);
 gitApp.AddBranch(branchMain);
 gitApp.AddBranch(branch2);
+gitApp.AddBranch(branch3);
+gitApp.AddBranch(branch4);
 gitApp.DeleteBranch(branch2);
 gitApp.DeleteUser(user3);
 #endregion
 #region branch
-
 FileContent content1 = new("this is my first content");
 FileContent content2 = new("this is my second content");
 FileContent content3 = new("this is my third content");
@@ -40,6 +57,17 @@ project.Composite.Folder folder1 = new("my first folder",4.2);
 project.Composite.Folder folder2 = new("my first folder", 4.2);
 project.Composite.Folder folder3 = new("my first folder", 4.2);
 project.Composite.Folder folder4 = new("my first folder", 4.2);
+folder1.AddItem(file1);
+folder1.AddItem(file2);
+folder1.AddItem(folder2);
+folder2.AddItem(file3);
+folder2.AddItem(folder3);
+folder3.AddItem(folder4);
+folder4.AddItem(file4);
+folder4.AddItem(file5);
+folder4.AddItem(file6);
+folder4.AddItem(file7);
+folder4.RemoveItem(file7);
 
 #endregion
 //to define list of branch item & user access
